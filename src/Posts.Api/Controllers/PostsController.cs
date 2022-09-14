88ff8969da077ab20a2f3c688dee0a409c23aa0b
Andre Vitor De Palma Lopes <andre.lopes@checkout.com>
2022-09-14 11:16:58 +0200

    Initial commit

---
+++ src/Posts.Api/Controllers/PostsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Posts.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PostsController : ControllerBase
+{
+    public PostsController()
+    {
+        
+    }
+}
